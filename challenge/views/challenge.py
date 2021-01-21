46bba7d682d926fe4062cc06e643ec4482d92f74
matheus <matheus@tradenamao.com.br>
2021-01-21 17:46:23 -0300

    remove unnecessary response

--- challenge/views/challenge.py
+++ challenge/views/challenge.py
@@ -4,10 +4,5 @@ from django.views import View
 
 class Challenge(View):
 
-    def get(self, request, data=None):
-        if not data:
-            data = {}
-        data['email'] = data.get('email', request.GET.get('email',''))
-        data['name'] = data.get('name', '')
-
-        return render(request, 'base.html', data)
+    def get(self, request):
+        return render(request, 'base.html')
