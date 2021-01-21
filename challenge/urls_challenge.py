a5e9d34e5d69507f3fcfe1d46ad34e88ca1dff3a
matheus <matheus@tradenamao.com.br>
2021-01-21 17:43:36 -0300

    remove urls_challenge

--- challenge/urls_challenge.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.conf import settings
-
-from challenge.views import Challenge
-
-urlpatterns = [
-    path('', Challenge.as_view, name='challenge'),
-]
